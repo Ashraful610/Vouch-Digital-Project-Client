6cc69e5f9f6f3c1f18c5fb0d3fb22dc7b5746f90
Ashraful610 <ashalam610@gmail.com>
2022-10-19 13:58:52 +0600

    install tailwind css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import SignIn from './Component/SignIn/SignIn';
 
 const App = () => {
   return (
     <div>
-      
+       <SignIn></SignIn>
     </div>
   );
 };
--- src/Component/SignIn/SignIn.js
+++ src/Component/SignIn/SignIn.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const SignIn = () => {
     return (
-        <div>
-            
+        <div className='w-full h-full'>
+            <div className='w-2/4 min-h-[500px] bg-black'>
+
+            </div>
+            <div className='w-2/4 min-h-[500px] h-full bg-blue-500'>
+
+            </div>
         </div>
     );
 };
