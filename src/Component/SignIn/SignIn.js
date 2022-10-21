3bdb59a2dcd21dbcc05908b4e7582f2a8824962c
Ashraful610 <ashalam610@gmail.com>
2022-10-21 10:59:14 +0600

    form design done

--- src/Component/SignIn/SignIn.js
+++ src/Component/SignIn/SignIn.js
@@ -14,29 +14,44 @@ const SignIn = () => {
    
 
     return (
-        <div className='w-full min-h-screen'>
-            <div className='w-2/4 min-h-screen flex justify-center items-center '>
-                    <form  onSubmit={handleSubmit(handleSignIn)} className='w-[300px] space-y-4'>
-                         <div className='text-center w-full '>
-                            <h2 className='text-black text-3xl font-serif font-semibold'>Welcome</h2>
-                            <h3 className='text-slate-600'>Enter your Username and Passoword</h3>
+        <div className='w-full'>
+            <div className='md:w-2/5 w-full min-h-screen flex justify-center items-center bg-teal-100'>
+                {/* ---------- form contaier --------------------- */}
+                 <div className='md:w-[80%] w-[90%] min-h-[550px] h-screen flex flex-col justify-between '>
+                     <form  onSubmit={handleSubmit(handleSignIn)} className=' space-y-4 mt-[20%]'>
+                         <div className='text-center w-full space-y-2'>
+                            <h2 className='text-black text-3xl font-sans font-semibold'>Welcome</h2>
+                            <h3 className='text-slate-500'>Enter your Username and Passoword</h3>
                          </div>
                          {/* ------- email input div ------------- */}
                          <div className='border-2 rounded flex justify-around p-2'>
                             <PersonOutlineOutlinedIcon className='text-slate-300'/>
-                            <input type='text' {...register("name" ,{required:true ,})} placeholder='Username' className='outline-0 placeholder:font-serif w-[90%]'/>   
+                            <input type='text' {...register("name" ,{required:true ,})} placeholder='Username' className='outline-0  w-[90%]'/>   
                          </div>
                          {/* ------- password input div ------------- */}
                          <div  className='border-2 rounded flex justify-around p-2'>
                             <LockOutlinedIcon className='text-slate-300'/>
-                            <input type={show ? 'text':'password'} {...register("password" ,{required:true} )}  placeholder='Password' className='w-[80%] placeholder:font-serif outline-0 '/>   
+                            <input type={show ? 'text':'password'} {...register("password" ,{required:true} )}  placeholder='Password' className='w-[80%] outline-0 '/>   
                             {/* --------- handle password show and hide ---------- */}
                              { 
                               show ?<VisibilityOffOutlinedIcon onClick={()=>setShow(!show)} className='text-slate-300'/>: <VisibilityOutlinedIcon onClick={()=>setShow(!show)} className='text-slate-300'/> 
                              }
                          </div>
-                            <button type='submit' className='btn'> Sign in</button>
-                    </form>
+                         {/* --------- log in button and forget password text */}
+                         <div className='flex items-center flex-col space-y-2'>
+                            <button type='submit' className='w-full rounded bg-blue-800 text-white p-2'> Login
+                            </button>
+                            <h3 className='text-slate-500 font-semibold'>Forget password?</h3>
+                         </div>
+                     </form>
+                           {/* ------------- bottom text -------------------- */}
+                      <div className='text-center h-fit mb-[5%] '>
+                           <h2> Terms of Use Privacy Policy</h2>
+                           <h3>© Punctualiti 2022. All rights reserved</h3>
+                      </div>
+                 </div>
+                  
+                   
             </div>
             
         </div>
